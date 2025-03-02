e6f7b737814962169aa182949982db455935ff6a
HimanshuHeda <harsh967896@gmail.com>
2025-03-03 02:01:39 +0530

    Working on SwipDrawer

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+
+// Components
+import Home from './Components/Home/Home';
+import SwipeDrawer from './SwipeDrawer';
+
+const Home = () => {    
+    return (
+        <SwipeDrawer />
+    );
+}
+
+export default Home;
